6635b2aaa1619ef550d5268161c0cea4ca080bcb
John Bozarov <jbozarov@teksystems.com>
2021-01-26 21:12:41 -0600

    dependencies changed, snapshot test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+configure({ adapter : new Adapter() })
+
+it("should create snapshot", () => {
+  const component = shallow(<App />)
+  expect(component).toMatchSnapshot(); 
+})
